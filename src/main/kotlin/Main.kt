6a663449dd6f3be3c6121af189c7a31a17e734e9
Mercy-chemtai <chemtailodite@gmail.com>
2024-02-07 09:17:58 +0300

    First Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    var fullName  = "Mercy Chemtai"
+    println (fullName)
+    var age = 22
+    println(age)
+    var phonenumber = +254112580268
+    println(phonenumber)
+    var weight = 64
+    println(weight)
+    var citizen = "true"
+    val nonCitizen = "false"
+    println(citizen)
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
